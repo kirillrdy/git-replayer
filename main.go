5bea66fd7969b0489907019b26fe4591681a41f3
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2022-06-14 19:05:37 +1000

    Also print how long it takes

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 func crash(err error) {
@@ -19,11 +20,13 @@ func main() {
 	crash(err)
 	commits := strings.Split(string(out), "\n")
 	for i, commit := range commits {
-		log.Printf("%d", i)
+		log.Printf("%d %s", i, commit)
+		start := time.Now()
 		err := exec.Command("git", "checkout", commit).Run()
 		crash(err)
 
 		err = exec.Command("nix", "build", ".#awsebcli").Run()
+		log.Printf("%d %s", i, time.Since(start).String())
 		crash(err)
 	}
 }
