695983b0e90b79b56489642afe1d7b59af7c265d
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2022-07-10 19:30:55 +1000

    Better flake

--- flake.nix
+++ flake.nix
@@ -3,16 +3,13 @@
 
   outputs = { self, nixpkgs }:
     let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-      };
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
     {
 
-      devShell.x86_64-linux = pkgs.mkShell {
-        buildInputs = [ pkgs.gopls ];
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ gopls go ];
       };
-      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
       defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation {
         name = "git-replayer";
